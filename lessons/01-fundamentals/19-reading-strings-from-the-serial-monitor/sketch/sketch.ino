81fefe3cb85b099b67755529ffff293a4221ff81
RaghavSemwal <raghav9tha35@gmail.com>
2026-07-28 19:38:22 +0530

    Arduino Tutorial 19: Reading Strings from the Serial Monitor

--- lessons/01-fundamentals/19-reading-strings-from-the-serial-monitor/sketch/sketch.ino
+++ lessons/01-fundamentals/19-reading-strings-from-the-serial-monitor/sketch/sketch.ino
@@ -6,12 +6,57 @@
   TODO: write/paste your code for this lesson here.
 */
 
-void setup() {
-  // put your setup code here, to run once:
+// Control RGB LED using Serial input
+// User types a colour name: red / green / blue
 
+
+int redpin = 2;
+int greenpin = 3;
+int bluepin = 4;
+
+String mycolour;
+
+void setup() {
+  // Set LED pins as OUTPUT
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+
+  // Start Serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  Serial.println("What colour LED do you want to blink?");
+  
+  // Wait until user enters something in Serial Monitor
+  while (Serial.available() == 0) {
+  }
+
+  // Read the entered colour
+  mycolour = Serial.readString();
+  mycolour.trim();   // Removes spaces/newline characters
+
+  // Turn OFF all LEDs first
+  digitalWrite(redpin, LOW);
+  digitalWrite(greenpin, LOW);
+  digitalWrite(bluepin, LOW);
+
+  // Compare strings using == (NOT =)
+  if (mycolour == "red") {
+    digitalWrite(redpin, HIGH);
+  }
+  else if (mycolour == "green") {
+    digitalWrite(greenpin, HIGH);
+  }
+  else if (mycolour == "blue") {
+    digitalWrite(bluepin, HIGH);
+  }
+  else {
+    Serial.println("Invalid colour! Type red / green / blue");
+  }
+
+  delay(500); // Small delay
 }
+
