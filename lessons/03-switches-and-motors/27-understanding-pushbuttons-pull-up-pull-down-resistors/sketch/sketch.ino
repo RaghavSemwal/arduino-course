853a4b26c241459a9310a7f32e12a5abade964a2
RaghavSemwal <raghav9tha35@gmail.com>
2026-08-02 00:35:12 +0530

    Arduino Tutorial 19: Reading Strings from the Serial Monitor

--- lessons/03-switches-and-motors/27-understanding-pushbuttons-pull-up-pull-down-resistors/sketch/sketch.ino
+++ lessons/03-switches-and-motors/27-understanding-pushbuttons-pull-up-pull-down-resistors/sketch/sketch.ino
@@ -6,12 +6,35 @@
   TODO: write/paste your code for this lesson here.
 */
 
-void setup() {
-  // put your setup code here, to run once:
+// Arduino Tutorial 27: Understanding Pushbuttons and Pull-Up/Down Resistors
 
+// Define pin numbers
+int buttonPin = 8;   // Pin connected to the push button
+int ledPin = 12;     // Pin connected to the LED
+
+void setup() {
+  // Configure pin modes
+  pinMode(buttonPin, INPUT);   // Button pin as input
+  pinMode(ledPin, OUTPUT);     // LED pin as output
+  
+  // Start serial communication for debugging
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read the current state of the button (HIGH or LOW)
+  int buttonState = digitalRead(buttonPin);
+  
+  // Print button state to Serial Monitor (0 = LOW, 1 = HIGH)
+  Serial.println(buttonState);
+  delay(100); // Small delay for readability
+  
+  // Control LED based on button state
+  if (buttonState == LOW) {    
+    // Button pressed (LOW when using pull-up resistor)
+    digitalWrite(ledPin, HIGH); // Turn LED ON
+  } else {
+    // Button not pressed
+    digitalWrite(ledPin, LOW);  // Turn LED OFF
+  }
 }
