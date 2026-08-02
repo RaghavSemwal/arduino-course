1dbdcbf3986052cac303ce927e9c318f0da1ca56
RaghavSemwal <raghav9tha35@gmail.com>
2026-08-02 16:07:24 +0530

    Arduino Tutorial 19: Reading Strings from the Serial Monitor

--- lessons/03-switches-and-motors/29-using-push-buttons-to-create-a-dimmable-led/sketch/sketch.ino
+++ lessons/03-switches-and-motors/29-using-push-buttons-to-create-a-dimmable-led/sketch/sketch.ino
@@ -6,12 +6,74 @@
   TODO: write/paste your code for this lesson here.
 */
 
-void setup() {
-  // put your setup code here, to run once:
+// -----------------------------
+// Dimmable LED with Push Buttons + Buzzer Alert
+// -----------------------------
 
+// Define pin numbers
+int buttonPin1 = 12;   // Button 1 increases brightness
+int buttonPin2 = 11;   // Button 2 decreases brightness
+int LEDPin     = 3;    // LED connected to PWM pin 3
+int buzzPin    = 2;    // Buzzer connected to pin 2
+
+// Variables to store button states and LED brightness
+int buttonVal1 = 0;
+int buttonVal2 = 0;
+int LEDBright  = 0;    // Start with LED off (brightness = 0)
+
+void setup() {
+  // Set button pins as input
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+
+  // Set LED and buzzer pins as output
+  pinMode(LEDPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+
+  // Start serial communication for debugging
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read button states
+  buttonVal1 = digitalRead(buttonPin1);
+  buttonVal2 = digitalRead(buttonPin2);
 
+  // Print button states to Serial Monitor
+  Serial.print("Button 1 = ");
+  Serial.print(buttonVal1);
+  Serial.print(", ");
+  Serial.print("Button 2 = ");
+  Serial.println(buttonVal2);
+
+  // Small delay to avoid too fast changes
+  delay(100);
+
+  // If Button 1 is pressed (LOW when pressed if using pull-down resistor)
+  if (buttonVal1 == LOW) {
+    LEDBright = LEDBright + 5;   // Increase brightness
+  }
+
+  // If Button 2 is pressed
+  else if (buttonVal2 == LOW) {
+    LEDBright = LEDBright - 5;   // Decrease brightness
+  }
+
+  // Limit brightness to valid PWM range (0–255)
+  if (LEDBright > 255) {
+    LEDBright = 255;  // Cap at maximum brightness
+
+    // Buzzer alert when max brightness reached
+    digitalWrite(buzzPin, HIGH);
+    delay(200);
+    digitalWrite(buzzPin, LOW);
+  }
+
+  if (LEDBright < 0) {
+    LEDBright = 0;    // Cap at minimum brightness
+  }
+
+  // Write brightness value to LED using PWM
+  analogWrite(LEDPin, LEDBright);
 }
+
